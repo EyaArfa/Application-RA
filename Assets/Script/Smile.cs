8da66a26395f9a27560210c55d7ab01fc923ee0b
Eya Arfa <arfaaya0@gmail.com>
2020-12-04 18:37:59 +0100

    update upto 4.12.2020

---
+++ Assets/Script/Smile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Smile : MonoBehaviour
+{
+    public AudioClip smile;
+    public AudioSource face;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Smilingwaving()
+    {
+        
+        face.PlayOneShot(smile);
+    }
+}
